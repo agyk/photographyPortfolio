8eb6baccd599f1fee447b250cf562741620854fb
agyk <agines.kovacs@gmail.com>
2023-05-20 16:47:42 +0200

    about section img added

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ let natureSection = document.getElementById("nature")
 let portraitImgs = document.querySelectorAll(".portrait-img")
 let portraitSection = document.getElementById("people")
 
+let otherImgs = document.querySelectorAll(".other-img")
+let otherSection = document.getElementById("other")
+
 function changeBg(){
     let images=[
         'url("img/IMG_2581_v2.jpg")',
@@ -21,7 +24,7 @@ function changeBg(){
     
 }
 
-setInterval(changeBg,3000)
+//setInterval(changeBg,3000)
 
 
 
@@ -81,9 +84,39 @@ function displayBigImgPortrait(){
   
 }
 
+function displayBigImgOther(){
+    for(let i=0; i<otherImgs.length; i++){
+        let smImg=otherImgs[i]
+        smImg.addEventListener("click",function clickEvent(){
+            console.log("click")
+            console.log(smImg.src)
+         let bigImgContainer = document.createElement("div")
+         let bigImg = document.createElement("img")
+         otherSection.appendChild(bigImgContainer)
+         bigImgContainer.appendChild(bigImg)
+         bigImg.src=smImg.src
+
+         smImg.classList.remove("img-container")
+         bigImgContainer.className="bigImg"
+         bigImgContainer.style="display:block"
+         
+         let closeBtn = document.createElement("span")
+         closeBtn.className = "close"
+         closeBtn.innerText = "X"
+         bigImgContainer.appendChild(closeBtn)
+         closeBtn.onclick =()=>{
+            bigImgContainer.style.display="none"
+         }
+        })
+    }
+  
+}
+
 displayBigImgNature()
 displayBigImgPortrait()
+displayBigImgOther()
 
+/*prevent right click to images*/ 
 document.addEventListener("contextmenu", function(e){
     if (e.target.nodeName === "IMG") {
         e.preventDefault();
